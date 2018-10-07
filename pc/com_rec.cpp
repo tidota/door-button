d8e9ef861bba9466b6e3792e8b8793ec55a76ea8
Tetsuya Idota <tidota@hawaii.edu>
2018-10-07 09:04:33 -1000

    Update notif message

--- pc/com_rec.cpp
+++ pc/com_rec.cpp
@@ -108,7 +108,7 @@ int main(void)
         {
             // beep
             cout << "beep" << endl;
-            system("notify-send \"!!! ATTENTION !!!\\nSomeone pressed the door button\"");
+            system("notify-send -u critical \"!!! ATTENTION !!!\" \"Someone pressed the door button\"");
             system("cvlc --play-and-exit alarm.ogg");
         }
         else if (strcmp("email", res) == 0)
